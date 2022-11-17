fb991ebf4f8f27ed03997da7a1e37d88d66e6cbb
alexa <alexa@DESKTOP-8LFFKKV.dhcp.wustl.edu>
2022-11-17 12:54:25 -0600

    studio

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,60 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
+	private int day;
+	private int month;
+	private int year;
+	private boolean holiday;
+	
+	public Date(int theDay, int theMonth, int theYear, boolean theHoliday) {
+		day=theDay;
+		month=theMonth;
+		year=theYear;
+		holiday=theHoliday;
+	}
+	
+	public boolean getHoliday() {
+		return holiday;
+	}
+		
+	public int getDay() {
+		return day;
+	}
+	
+	public int getMonth() {
+		return month;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public String toString() {
+		return getMonth()+"."+getDay()+"."+getYear()+" Is a Holiday: "+getHoliday();
+	}
 
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+ 
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
 
-    public static void main(String[] args) {
-
+	public static void main(String[] args) {
+		Date d1= new Date(11,11,2001,true);
+		Date d2= new Date(11,11,2001,false);
+		System.out.println(d1.equals(d2));
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,10 +1,58 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class Time {
+	private int hour;
+	private int minute;
+	
+	public Time(int theHour, int theMinute) {
+		hour=theHour;
+		minute=theMinute;
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+	
+	public void militaryTime(boolean militarytime) {
+		if (militarytime=false) {
+			if (hour>12) {
+				hour=hour-12;
+			}
+		}
+	}
+
+	public String toString() {
+		return "Time: "+hour+":"+minute;
+	}
+
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
 
 	public static void main(String[] args) {
- 
+		LinkedList<Date> list= new LinkedList<Date>();
+		Time t1 = new Time(5,30);
+		Time t2 = new Time (6,30);
+		
+		
     	
     }
 
